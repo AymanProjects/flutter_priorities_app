2db91a1a5448cf0b7506b0b771be9276149d2c67
aymanprojects <aymanz.dev@gmail.com>
2022-05-30 10:53:25 +0300

    A standard View widget

--- lib/views/home/components/options_button.dart
+++ lib/views/home/components/options_button.dart
@@ -3,19 +3,14 @@ import 'package:priorities/views/__components/horizon_icon_button.dart';
 import 'package:priorities/views/home/home_view_model.dart';
 import 'package:stacked/stacked.dart';
 
-class OptionsViewButton extends ViewModelWidget<HomeViewModel> {
-  const OptionsViewButton({Key? key}) : super(key: key);
+class OptionsButton extends ViewModelWidget<HomeViewModel> {
+  const OptionsButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, viewModel) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        HorizonIconButton(
-          onTap: viewModel.optionsViewButtonOnTap,
-          icon: Icons.settings_rounded,
-        ),
-      ],
+    return HorizonIconButton(
+      onTap: viewModel.optionsViewButtonOnTap,
+      icon: Icons.settings_rounded,
     );
   }
 }
--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:priorities/utils/constants.dart';
+import 'package:priorities/views/__components/view.dart';
 import 'package:priorities/views/home/components/create_priority_button.dart';
 import 'package:priorities/views/home/components/options_button.dart';
 import 'package:priorities/views/home/components/priorities_grid.dart';
 import 'package:priorities/views/home/components/selected_category.dart';
-import 'package:priorities/views/home/components/title.dart';
 import 'package:priorities/views/home/home_view_model.dart';
 import 'package:stacked/stacked.dart';
 
@@ -18,30 +18,20 @@ class HomeView extends StatelessWidget {
     return ViewModelBuilder<HomeViewModel>.reactive(
       viewModelBuilder: () => HomeViewModel(),
       builder: (context, viewModel, child) {
-        return Padding(
-          padding: const EdgeInsets.only(
-            left: kLgPadding,
-            right: kLgPadding,
-            top: kLgPadding * 2,
-          ),
-          child: Scaffold(
-            body: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                OptionsViewButton(),
-                HomeViewTitle(),
-                SizedBox(height: kLgPadding),
-                SelectedCategory(),
-                SizedBox(height: kSmPadding),
-                Expanded(
-                  child: PrioritiesGrid(),
-                ),
-                SizedBox(height: kSmPadding),
-                CreatePriorityButton(),
-                SizedBox(height: kLgPadding),
-              ],
+        return const View(
+          title: 'Your Priorities',
+          hasBackButton: false,
+          trailing: OptionsButton(),
+          children: [
+            SelectedCategory(),
+            SizedBox(height: kSmPadding),
+            Expanded(
+              child: PrioritiesGrid(),
             ),
-          ),
+            SizedBox(height: kSmPadding),
+            CreatePriorityButton(),
+            SizedBox(height: kLgPadding),
+          ],
         );
       },
     );
